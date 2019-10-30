5a35215e3b4aa69b2ef9e2816a047c42737bfb26
Colin SKow <skow0020@gmail.com>
2019-10-30 11:05:12 -0500

    Clean jenkins scripts

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -11,12 +11,6 @@ echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 # npm install --save-dev cross-env
-set +x
-
-echo 'The following "npm" command tests that your simple Node.js/React'
-echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
-set -x
 
 npm run test:unit:backend
 
