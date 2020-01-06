37e3b8d2f5e114635c6e97d0ef5e55676a90418e
Colin SKow <skow0020@gmail.com>
2020-01-06 16:13:25 -0600

    Update main views to functional, hooks, material ui

---
+++ client/src/components/common/CardComponent.js
@@ -0,0 +1,62 @@
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Colors from 'utils/Colors';
+import PropTypes from "prop-types";
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { getCategoryTheme } from "utils/util";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  cardImage: {
+    position: "relative",
+    minHeight: "10.3125rem",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat"
+  }
+}));
+
+export default function ComponentCard(props) {
+  const classes = useStyles();
+  const { idx, url, title, subheader, category, backgroundImage, body, pdf  } = props;
+
+  return (
+    <Card id={idx}>
+      <a href={url} target="_blank" rel="noopener noreferrer" aria-label="Navigate to the article url" style={{ color: Colors.black }}>
+        <CardHeader
+          titleTypographyProps={{ variant: 'h6' }}
+          title={title}
+          subheader={subheader}
+          avatar={
+            <Avatar aria-label="category" style={{ backgroundColor: getCategoryTheme(category) }}>
+              {category.substring(0, 3)}
+            </Avatar>
+          }
+        />
+        <div
+          className={classes.cardImage}
+          style={{ backgroundImage: `url('${backgroundImage}')` }}
+        />
+      </a>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {body}
+        </Typography>
+        {pdf && <a className="text-fiord-blue" href={pdf} target="_blank" rel="noopener noreferrer">PDF Version</a>}
+      </CardContent>
+    </Card>
+  );
+}
+ComponentCard.propTypes = {
+  idx: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subheader: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string.isRequired,
+  body: PropTypes.string,
+  pdf: PropTypes.string
+};
--- client/src/views/Articles/Articles.js
+++ client/src/views/Articles/Articles.js
@@ -1,131 +1,95 @@
-/* eslint jsx-a11y/anchor-is-valid: 0 */
+/* eslint-disable react-hooks/exhaustive-deps */
 
-import {
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Col,
-  Container,
-  FormGroup,
-  Row
-} from "shards-react";
+import React, { useEffect, useState } from "react";
 
+import Button from '@material-ui/core/Button';
+import CardComponent from "components/common/CardComponent";
 import CategoriesSelection from "components/common/CategoriesSelection";
+import Colors from 'utils/Colors';
+import Grid from '@material-ui/core/Grid';
 import LanguagesSelection from "components/common/LanguagesSelection";
+import { Link } from 'react-router-dom';
 import LoadError from "components/common/LoadError";
 import Loading from "components/common/Loading";
 import PageTitle from "components/common/PageTitle";
-import React from "react";
-import { getCategoryTheme } from "utils/util";
+import { makeStyles } from '@material-ui/core/styles';
 
-class Articles extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      isLoading: false,
-      category: '',
-      language: ''
-    };
-
-    this.getArticles = this.getArticles.bind(this);
-    this.handleFilterChange = this.handleFilterChange.bind(this);
-    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+const useStyles = makeStyles(() => ({
+  addButton: {
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    marginLeft: "auto"
   }
+}));
 
-  componentDidMount() {
-    this.getArticles();
-  }
+export default function Articles() {
+  const classes = useStyles();
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [category, setCategory] = useState('');
+  const [language, setLanguage] = useState('');
+  const [error, setError] = useState('');
 
-  getArticles() {
-    const categoryFilter = this.state.category ? `category=${this.state.category}` : '';
-    const languageFilter = this.state.language ? `language=${this.state.language}` : '';
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  useEffect(() => { getArticles(); }, [category]);
+  useEffect(() => { getArticles(); }, [language]);
+
+  const getArticles = () => {
+    const categoryFilter = category ? `category=${category}` : '';
+    const languageFilter = language ? `language=${language}` : '';
     const filter = `${categoryFilter}&${languageFilter}`;
 
-    this.setState({ isLoading: true });
+    setIsLoading(true);
     fetch(`/api/articles?${filter}`)
       .then(response => response.json())
       .then(
-        data => this.setState({ articles: data.data, isLoading: false }),
-        error => this.setState({ error, isLoading: false })
+        data => {
+          setArticles(data.data);
+          setIsLoading(false);
+        },
+        error => {
+          setError(error);
+          setIsLoading(false);
+        }
       );
-  }
+  };
 
-  nextPath(path) {
-    this.props.history.push(path);
-  }
+  if (isLoading) return <Loading />;
 
-  handleFilterChange(e) {
-    this.setState({
-      category: e.target.value
-    }, () => this.getArticles());
-  }
+  if (error) return <LoadError error="Articles failed to load" />;
 
-  handleLanguageChange(e) {
-    this.setState({
-      language: e.target.value
-    }, () => this.getArticles());
-  }
-
-  render() {
-    const { articles, isLoading, error, category, language } = this.state;
-
-    if (isLoading) return <Loading />;
-
-    if (error) return <LoadError error="Articles failed to load" />;
-
-    return (
-      <Container fluid className="main-content-container px-3">
-        <Row noGutters className="form-inline py-2">
-          <PageTitle sm="8" title="Articles" className="text-sm-left" />
-          <Button id="add-article" type="button" className="btn btn-success btn-lg" onClick={() => this.nextPath('/add-article')}>
-            Add Article
-          </Button>
-        </Row>
-        <Row noGutters className="form-inline py-2">
-          <FormGroup id='filtering-form'>
-            <CategoriesSelection id="category" value={category} onChange={this.handleFilterChange} />
-            <LanguagesSelection id="language" value={language} onChange={this.handleLanguageChange} />
-          </FormGroup>
-        </Row>
-        <Row>
-          {articles.map((post, idx) => (
-            <Col lg="4" md="6" sm="12" className="mb-4" key={idx}>
-              <Card small id={`article-card-${idx}`} className="card-post card-post--1">
-                <a href={post.url} target="_blank" rel="noopener noreferrer" aria-label="Navigate to the article url">
-                  <div
-                    className="card-post__image"
-                    style={{ backgroundImage: `url('${post.backgroundImage}')` }}
-                  />
-                </a>
-                <Badge pill className={`card-post__category bg-${getCategoryTheme(post.category)}`}>
-                  {post.category}
-                </Badge>
-                <CardBody>
-                  <h5 className="card-title">
-                    <a className="text-fiord-blue" href={post.url} target="_blank" rel="noopener noreferrer" aria-label="Navigate to the article url">
-                      {post.title}
-                    </a>
-                  </h5>
-                  <p className="card-text">{post.body}</p>
-                </CardBody>
-                <CardFooter className="text-muted border-top py-3">
-                  <span className="d-inline-block">
-                    By{" "}
-                    <a className="text-fiord-blue">
-                      {post.author}
-                    </a>
-                  </span>
-                </CardFooter>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Grid container>
+      <Grid container alignItems="center" >
+        <PageTitle title="Articles" />
+        <Button id="add-article" component={Link} to="/add-article" variant="contained" className={classes.addButton}>
+          Add Article
+        </Button>
+      </Grid>
+      <Grid >
+        <form id='filtering-form'>
+          <CategoriesSelection id="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+          <LanguagesSelection id="language" value={language} onChange={(e) => setLanguage(e.target.value)} />
+        </form>
+      </Grid>
+      <Grid container spacing={4}>
+        {articles.map((post, idx) => (
+          <Grid item md={4} key={idx}>
+            <CardComponent
+              idx={`article-card-${idx}`}
+              url={post.url}
+              title={post.title}
+              subheader={`By ${post.author}`}
+              category={post.category}
+              backgroundImage={post.backgroundImage}
+              body={post.body}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Articles;
