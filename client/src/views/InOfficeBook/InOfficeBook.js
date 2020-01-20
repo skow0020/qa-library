970cbb71e278fca41d84e28f94256f0460ccae23
Colin <skow0020@gmail.com>
2020-01-20 15:49:50 -0600

    Add material ui to InOfficeBook

--- client/src/views/InOfficeBook/InOfficeBook.js
+++ client/src/views/InOfficeBook/InOfficeBook.js
@@ -1,49 +1,72 @@
-import AlertModal, { showAlert } from "components/common/AlertModal";
-import {
-  Badge,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Container,
-  Row
-} from "shards-react";
+/* eslint-disable react-hooks/exhaustive-deps */
 
+import AlertModal, { showAlert } from "components/common/AlertModal";
+
+import Button from "@material-ui/core/Button";
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Chip from '@material-ui/core/Chip';
+import Container from '@material-ui/core/Container';
+import CardMedia from '@material-ui/core/CardMedia';
+import Divider from '@material-ui/core/Divider';
+import Colors from 'utils/Colors';
+import Typography from '@material-ui/core/Typography';
 import LoadError from "components/common/LoadError";
 import Loading from "components/common/Loading";
-import PageTitle from "components/common/PageTitle";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Store } from "../../flux";
 import axios from "axios";
 import { getCategoryTheme } from "utils/util";
 
-class InOfficeBook extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: false,
-      book: {}
-    };
+import { makeStyles } from '@material-ui/core/styles';
 
-    this.handleCheckout = this.handleCheckout.bind(this);
-    this.handleCheckIn = this.handleCheckIn.bind(this);
+const useStyles = makeStyles((theme) => ({
+  checkInButton: {
+    backgroundColor: Colors.primary,
+    color: Colors.white
+  },
+  checkOutButton: {
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    marginLeft: "auto",
+    margin: theme.spacing(0, 4)
+  },
+  checkOutButtons: {
+    margin: theme.spacing(2, 2)
   }
+}));
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    if (this.props.match.params.office_book_id)
-    {
-      fetch(`/api/officeLibraryBooks/${this.props.match.params.office_book_id}`)
+export default function InOfficeBook(props) {
+  const classes = useStyles();
+  const [book, setBook] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getBook();
+  }, []);
+
+  const getBook = () => {
+    setIsLoading(true);
+    const { office_book_id } = props.match.params;
+    fetch(`/api/officeLibraryBooks/${office_book_id}`)
       .then(response => response.json())
       .then(
-        data => this.setState({ book: data.data[0], isLoading: false, users: data.data[0] }),
-        error => this.setState({ error, isLoading: false })
+        data => {
+          if (data.error) setError(data.error);
+          else setBook(data.data[0]);
+          setIsLoading(false);
+        },
+        error => {
+          setError(error);
+          setIsLoading(false);
+        }
       );
-    }
-    
-  }
+  };
 
-  handleCheckout(book_id) {
+  const handleCheckout = (book_id) => {
     const checkoutBook = {
       office_book_id: book_id,
       user: Store.user
@@ -51,14 +74,14 @@ class InOfficeBook extends React.Component {
     axios.patch("/api/officeLibraryBooks/incrementCopiesCheckedOut", checkoutBook)
       .then(
         data => {
-          if (!data.data.success) this.showNotifier(`Unable to check out book: ${data.data.error}. You may have to log in again.`);
-          else this.componentDidMount();
+          if (!data.data.success) showAlert({ message: `Unable to check out book: ${data.data.error}. You may have to log in again.` });
+          else getBook();
         },
-        error => this.showNotifier(`Unable to checkout book: ${error}`)
+        error => showAlert({ message: `Unable to checkout book: ${error}` })
       );
-  }
+  };
 
-  handleCheckIn(book_id) {
+  const handleCheckIn = (book_id) => {
     const checkinBook = {
       office_book_id: book_id,
       user: Store.user
@@ -66,73 +89,66 @@ class InOfficeBook extends React.Component {
     axios.patch("/api/officeLibraryBooks/decrementCopiesCheckedOut", checkinBook)
       .then(
         data => {
-          if (!data.data.success) this.showNotifier(`Unable to check out book: ${data.data.error}`);
-          else this.componentDidMount();
+          if (!data.data.success) showAlert({ message: `Unable to check out book: ${data.data.error}` });
+          else getBook();
         },
-        error => this.showNotifier(`Unable to check in book: ${error}`)
+        error => showAlert({ message: `Unable to check in book: ${error}` })
       );
-  }
+  };
 
-  getUsers = () => {
-    if (this.state.book.users) return this.state.book.users.join(', ');
-  }
+  const getUsers = () => {
+    return book.users ? book.users.join(', ') : "No one";
+  };
 
-  showNotifier = (message) => {
-    showAlert({ message });
-  }
+  if (isLoading) return <Loading />;
 
-  render() {
-    const { isLoading, error, book } = this.state;
+  if (error) return <LoadError error="Unable to find library book" />;
 
-    if (isLoading) return <Loading />;
+  return (
+    <Container className="card-post">
+      <AlertModal />
+      <Grid>
+        <Card>
+          <CardHeader
+            titleTypographyProps={{ variant: 'h6' }}
+            title={book.title}
+            avatar={
+              <Chip
+                label={book.category}
+                style={{ backgroundColor: getCategoryTheme(book.category) }} />
+            }
+          />
+          <CardMedia
+            component="img"
+            alt="Book image"
+            height="250"
+            image={book.backgroundImage}
+            title={book.title}
+          />
+          <CardContent className="text-muted border-top py-3">
+            <div className={classes.bookInfo}>
+              <Typography variant="body1" color="textPrimary" paragraph >
+                {book.body}
+              </Typography>
+              <Grid>
+                <Typography variant="body1" color="textSecondary" gutterBottom >
+                  By{" "}{book.author} | {book.totalCopies - book.copiesCheckedOut}{" "}Available | Checked out by: {getUsers()}
+                </Typography>
+              </Grid>
+            </div>
+            <Divider variant="middle" />
+            <div className={classes.checkOutButtons}>
+              <Button className={`checkout-${book.office_book_id} ${classes.checkOutButton}`} variant="contained" onClick={() => handleCheckout(book.office_book_id)}>
+                Check out
+              </Button>
 
-    if (error) return <LoadError error="Library book failed to load" />;
-
-    return (
-      <Container fluid className="main-content-container px-3">
-        <Row noGutters className="form-inline py-4">
-          <PageTitle sm="8" title={book.title} className="text-sm-left" />
-        </Row>
-        <Row>
-          <Card small={false} id={`book-card`} className="card-book card-post--1">
-            <div
-              className="card-post__image"
-              style={{ backgroundImage: `url('${book.backgroundImage}')` }}
-            />
-            <Badge pill className={`card-post__category bg-${getCategoryTheme(book.category)}`}>
-              {book.category}
-            </Badge>
-            <CardBody>
-              <h5 className="card-title">
-                <p className="text-fiord-blue">
-                  {book.title}
-                </p>
-              </h5>
-              <p className="card-text text-fiord-blue">{book.body}</p>
-            </CardBody>
-            <CardFooter className="text-muted border-top py-3">
-              <Row>
-                <span className="d-inline-block px-3">
-                  By{" "}{book.author} | {book.totalCopies - book.copiesCheckedOut}{" "}Available | Checked out by: {this.getUsers()}
-                </span>
-              </Row>
-              <div>
-                <Button type="button" className={`checkout-${book.office_book_id} btn btn-success`} onClick={() => this.handleCheckout(book.office_book_id)}>
-                  Check out
-                </Button>
-                <span className="d-inline-block px-3">
-                  {" "}
-                </span>
-                <Button type="button" className={`checkin-${book.office_book_id} btn btn-success pull-right`} onClick={() => this.handleCheckIn(book.office_book_id)}>
-                  Check in
-                  </Button>
-              </div>
-            </CardFooter>
-          </Card>
-        </Row>
-        <AlertModal />
-      </Container>
-    );
-  }
+              <Button className={`checkin-${book.office_book_id} ${classes.checkInButton}`} variant="contained" onClick={() => handleCheckIn(book.office_book_id)}>
+                Check in
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Container >
+  );
 }
-export default InOfficeBook;
