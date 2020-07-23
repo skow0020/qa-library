3367ee6ac97cfe01efee7a9e1230637a3335d383
Colin SKow <skow0020@gmail.com>
2020-07-23 17:00:18 -0500

    Fix linting error in footer

--- client/src/components/layout/MainFooter.js
+++ client/src/components/layout/MainFooter.js
@@ -1,7 +1,6 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import React from 'react';
-import { Route } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import styles from 'assets/jss/components/footerStyle.js';
 
