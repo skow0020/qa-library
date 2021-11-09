c034c7e8c59fae3a28dccb7e0054774afcb80aef
Colin Gitter <skow0020@gmail.com>
2021-11-09 13:59:08 -0600

    lighthouse try

--- client/lighthouserc.js
+++ client/lighthouserc.js
@@ -1,5 +1,8 @@
 module.exports = {
     ci: {
+        collect: {
+            staticDistDir: './client/build/',
+        },
         upload: {
             target: 'temporary-public-storage',
         },
