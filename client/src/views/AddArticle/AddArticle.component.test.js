21a16a78424e4e05ddd1270d88c1b56cb0a1ae42
Colin Skow <colin.skow@solutiondesign.com>
2022-02-07 14:17:52 -0600

    Lint fixes

--- client/src/views/AddArticle/AddArticle.component.test.js
+++ client/src/views/AddArticle/AddArticle.component.test.js
@@ -5,15 +5,15 @@ import { mount } from '@cypress/react';
 it('AddArticle renders', () => {
   mount(<AddArticle />);
   cy.get('.page-title').contains('Add an Article');
-  cy.get('#title').type('titlee')
-  cy.get('#author').type('titlee')
-  cy.get('#url').type('titlee')
-  cy.get('#backgroundImage').type('titlee')
-  cy.get('#title').type('titlee')
+  cy.get('#title').type('titlee');
+  cy.get('#author').type('titlee');
+  cy.get('#url').type('titlee');
+  cy.get('#backgroundImage').type('titlee');
+  cy.get('#title').type('titlee');
 
   cy.intercept('POST', '**/api/articles', { statusCode: 201, fixture: 'article.json' });
 
-  cy.get('#submit-button').contains('Submit').click()
-  cy.contains('Article added successfully')
+  cy.get('#submit-button').contains('Submit').click();
+  cy.contains('Article added successfully');
 });
 
