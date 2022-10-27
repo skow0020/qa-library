204ec85b6633c5695953cc47800bb54cdae1f0fd
Colin Skow <colin.skow@solutiondesign.com>
2022-10-27 11:19:24 -0500

    Add LibraryDash back

--- client/src/routes.js
+++ client/src/routes.js
@@ -11,8 +11,11 @@ import IconFolderOpen from '@material-ui/icons/FolderOpen';
 import IconGitHub from '@material-ui/icons/GitHub';
 import IconLibraryBooks from '@material-ui/icons/LibraryBooks';
 import IconLink from '@material-ui/icons/Link';
+import IconLocalLibrary from '@material-ui/icons/LocalLibrary';
 import IconQuestionAnswer from '@material-ui/icons/QuestionAnswer';
+import InOfficeBook from 'views/InOfficeBook/InOfficeBook';
 import Info from 'views/Info/Info';
+import LibraryDash from 'views/LibraryDash/LibraryDash';
 import NotFound from 'components/common/NotFound';
 import QADashboard from 'views/QADashboard/QADashboard';
 import ResourceLinks from 'views/ResourceLinks/ResourceLinks';
@@ -103,6 +106,19 @@ const routes = [
     layout: DefaultLayout,
     element: Search,
     name: 'Search'
+  },
+  {
+    path: '/library',
+    layout: DefaultLayout,
+    element: LibraryDash,
+    name: 'Library Dashboard',
+    icon: IconLocalLibrary
+  },
+  {
+    path: '/officeBook/:office_book_id',
+    layout: DefaultLayout,
+    component: InOfficeBook,
+    name: 'In-Office Book'
   },
   {
     path: '*',
